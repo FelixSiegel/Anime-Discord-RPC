2c7492001d497f89bde83b3ac82dd09fe1834e89
Felix Siegel <ninja.zero@seznam.cz>
2023-02-18 20:08:36 +0100

    Improved flaskserver file No url-error is fixed. Switching host is now correct. Further some small exception improvments

--- DCM_flask-server.py
+++ DCM_flask-server.py
@@ -13,53 +13,84 @@ import time
 from flask import *
 from flask_cors import CORS
 
-CLIENT_ID = "1020359247059497071"
+APPLICATION_IDs = {
+    'aniworld': '1020359247059497071',
+    'crunchyroll': '1076049094281277531'
+}
+rpc_aniworld = Presence(APPLICATION_IDs['aniworld'])
+rpc_crunchy = Presence(APPLICATION_IDs['crunchyroll'])
+
 app = Flask(__name__)
 CORS(app, resources={r"/rpc_anime": {"origins": "*"}})
-RPC = Presence(CLIENT_ID)
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/rpc')  
-def rpc_default():  
+
+@app.route('/rpc')
+def rpc_default():
     return render_template('rpc_default.html')
-  
-@app.route('/rpc_anime',methods = ['POST', 'GET'])  
-def rpc_anime():  
+
+
+@app.route('/rpc_anime', methods=['POST', 'GET'])
+def rpc_anime():
     if request.method == "POST":
         print("\033[92m[INFO]:\033[00m Getting datas from rpc_anime.html", file=sys.stdout)
         result = request.get_json()
-        
+
         if result["type"] == "update":
-            # TODO: Change between hosts
-            RPC.update(
-                large_image=result["host"],
-                large_text=f"{result['host']} Logo",
-                details=result["details"],
-                state=result["state"],
-                start=int(time.time()),
-                buttons=[{"label":"My AniList", "url":result["anilist"]}]
+            # args include all parameter for the rpc.update()-Function
+            args = [
+                result["host"],
+                f"{result['host']} logo",
+                result["details"], result["state"],
+                int(time.time())]
+            args.append(
+                None if result["anilist"] == "" else [
+                    {"label": "My AniList", "url": result["anilist"]}]
             )
-            print("\033[92m[INFO]:\033[00m Started Disord RPC with Anime", file=sys.stdout)
-        else:
+            print(args)
+            if result["host"] == "aniworld":
+                rpc_aniworld.update(large_image=args[0], large_text=args[1], 
+                                    details=args[2], state=args[3], start=args[4], buttons=args[5])
+                print(f"\033[92m[INFO]:\033[00m Started Disord RPC with {result['host']}", file=sys.stdout)
+                # clear possible open connection to crunchyroll-application
+                rpc_crunchy.clear()
+            elif result["host"] == "crunchyroll":
+                rpc_crunchy.update(large_image=args[0], large_text=args[1],
+                                   details=args[2], state=args[3], start=args[4], buttons=args[5])
+                print(f"\033[92m[INFO]:\033[00m Started Disord RPC with {result['host']}", file=sys.stdout)
+                # clear possible open connection to aniworld-application
+                rpc_aniworld.clear()
+            else:
+                print(f"\033[91m[ERROR]:\033[00m No valid Host: {result['host']}")
+
+        elif result["type"] == "clear":
             try:
-                RPC.clear()
+                rpc_aniworld.clear()
+                rpc_crunchy.clear()
                 print("\033[92m[INFO]:\033[00m \033[33mStopped RPC\033[00m", file=sys.stdout)
             except Exception:
-                print("\033[91m[ERROR]:\033[00m Cant connect with Discord Gateway... Try reconnect automatically.")
-                RPC.connect()
+                print("\033[91m[ERROR]:\033[00m No connection to Discord Gateway... Try reconnect automatically.")
+                rpc_aniworld.connect()
+                rpc_crunchy.connect()
+        else: 
+            print("\033[91m[ERROR]:\033[00m No valid type was given in json from request")
+            return jsonify({'processed': 'false'})
 
-        response_data = jsonify({'processed': 'true'})
-        return response_data
+        return jsonify({'processed': 'true'})
     return render_template('rpc_anime.html')
-  
-if __name__ =='__main__':  
+
+
+if __name__ == '__main__':
     print("\033[92m[INFO]:\033[00m Connect to Discord RPC")
-    RPC.connect()
+    rpc_aniworld.connect()
+    rpc_crunchy.connect()
     print("\033[92m[INFO]:\033[00m Start Flask server on port 8000")
     app.run(port=8000)
     print("\033[91m[STOPPED]:\033[00m Shutdown Server", file=sys.stdout)
-    RPC.close()
+    rpc_aniworld.close()
+    rpc_crunchy.close()
     print("\033[91m[STOPPED]:\033[00m Closed connection to Discord Gateway", file=sys.stdout)
